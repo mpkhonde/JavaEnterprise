e21d14ccfe4b17c94c7ff6c4767f9c4de399426e
mpkhonde <91606609+mpkhonde@users.noreply.github.com>
2025-10-07 22:27:25 +0200

    Lade till Uppgift 3

---
+++ EE_uppgifter/Uppgift3/EE_uppgifter/Uppgift3/Uppgift3Main.java
@@ -0,0 +1,8 @@
+package EE_uppgifter.Uppgift3;
+
+
+public class Uppgift3Main {
+    public static void main(String[] args) {
+        System.out.println("Uppgift 3 fungerar som den ska!");
+    }
+}
